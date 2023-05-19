a65dce100db66766abaa3538546223fd853df019
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-05-18 18:14:39 -0700

    task 2 fixed printing error

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,26 +7,26 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, k = 0;
-	int aux = 0, is_min = 0;
+	size_t i, j, idx_min = 0;
+	int aux = 0;
 
 	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		is_min = array[i];
+		idx_min = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (is_min > array[j])
-			{
-				is_min = array[j];
-				k = j;
-			}
+			if (array[j] < array[idx_min])
+				idx_min = j;
 		}
-		aux = array[i];
-		array[i] = is_min;
-		array[k] = aux;
-		print_array(array, size);
+		if (idx_min != i)
+		{
+			aux = array[i];
+			array[i] = array[idx_min];
+			array[idx_min] = aux;
+			print_array(array, size);
+		}
 	}
 }
